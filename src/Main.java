8ea7399a996d340f311cb0a5792e1c89e23c6fcd
vlifanova <vlifanova2@alfabank.ru>
2022-05-04 10:51:22 +0300

    Calculator

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Vera Lifanova
+ * Date: 04.05.2022
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b);
+
+        calc.println.accept(c);
+    }
+}
