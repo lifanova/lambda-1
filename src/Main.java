a8ec4640ece1184130f36ca8a70b029b6303ce29
vlifanova <vlifanova2@alfabank.ru>
2022-05-04 14:52:05 +0300

    Calculator

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1,1);
+        int b = calc.minus.apply(1, 1);
 
         try {
             int c = calc.devide.apply(a, b);
